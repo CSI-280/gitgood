ad88c15310a29455095abf0dbcde91e6224155f9
JoshBogin <joshbogin1@gmail.com>
2020-02-19 23:58:58 -0500

    updated and add table and info to cats page.

--- src/components/Cats.vue
+++ src/components/Cats.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-on:load="getCats(), getToken()">
 
     <div id="body">
         <div class="container">  
@@ -15,48 +15,81 @@
           <router-link to="/Other">
             <button>Other</button>
           </router-link>
-        </div>
-        <div id="pfData">
+          <div id="pfData">
+          </div>
+          <button v-on:click="getCats(), getToken()">Generate</button>
+          <p>Contact Us For Help and More Information About Our Pets!</p>
         </div>
     </div>
 </template>
 
-<script src="https://code.jquery.com/jquery-3.4.1.js">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/@petfinder/petfinder-js/dist/petfinder.min.js"></script>
+<script src="https://unpkg.com/@petfinder/petfinder-js/dist/petfinder.min.js%22%3E"></script>
+
+<script>
   import { Client } from "@petfinder/petfinder-js";
   import { Animal } from '@petfinder/petfinder-js/dist/api/animal';
 
-  export default {
+export default {
   name: 'Cats',
   data () {
       return {
       msg: 'Catspage'
       }
   },
-      methods: {
-          getToken: function () {
-              const client = new Client({ apiKey: "HB4E0LPBodtgXJlBVOYvZSDaxgGSCA7Li7Eq6tqb6uVDRfzAp4", secret: "ACCq1YmJ2XzZxT6WrvbSU9voepnbCllw0NmSF363" });
-              var output = document.getElementById("pfData");
-              format = JSON;
+    methods: {
+        getToken: function () {
+            const client = new Client({ apiKey: "HB4E0LPBodtgXJlBVOYvZSDaxgGSCA7Li7Eq6tqb6uVDRfzAp4", secret: "rr6C5OrDLoCg3mP0rk4ztxVTLw3sHwgmvUUMf3zn" });
+            client.animal.search()
+                .then(function (response) {
+                    // Do something with resp.data.animals
+                    console.dir(response.data.animals)
+                });
+        },
+        getCats: function () {
+          const client = new Client({ apiKey: "HB4E0LPBodtgXJlBVOYvZSDaxgGSCA7Li7Eq6tqb6uVDRfzAp4", secret: "rr6C5OrDLoCg3mP0rk4ztxVTLw3sHwgmvUUMf3zn" });
+          var tableOutput = document.getElementById("pfData");
+          
+          console.log("made it....");
 
-              $(document).ready(function () {
-                $.getJSON(Client, function(data) {
-                  var cat_data = '';
-                  $.each(data, function(key, value) {
-                    cat_data += '<tr>';
-                    cat_data += '<td>' + value.name + '</td>';
-                    cat_data += '<td>' + value.animal + '</td>';
-                    cat_data += '<td>' + value.breed + '</td>';
-                    cat_data += '<td>' + value.shelterid + '</td>';
-                    cat_data += '<td>' + value.primaryphoto + '</td>';
-                    cat_data += '</tr>';
-                  })
-                })
-              });
-              console.log("hi")
-              console.log(client$pet.getRandom(output = 'basic'))
-          }
-      }
+          client.animal.search()
+          .then(function (response){
+            tableOutput.innerHTML = "";
+            for (var i = 0; i < response.data.animals.length; i++ )
+            {
+              var item = response.data.animals[i];
+              if (item.type == 'Cat')
+              {
+                if (item.photos.length > 0) {
+                  var petPic = item.photos[0].medium;
+                  tableOutput.innerHTML += "<img src=" + petPic + ">";
+                }
+                var petName = item.name;
+                var petID = item.id;
+                var breed = item.breeds.primary;
+                var petSize = item.size;
+                var petAge = item.age;
+                var petDescription = item.description;
+                tableOutput.innerHTML += "<h1>" + petName + "</h1>";
+                tableOutput.innerHTML += "<h3>Age: " + petAge + "</h3>";
+                tableOutput.innerHTML += "<h3>Breed: " + breed + "</h3>";
+                tableOutput.innerHTML += "<h3>Size: " + petSize + "</h3>";
+                tableOutput.innerHTML += "<h3>" + petDescription + "</h3><br>";
+              }
+              else
+              {
+                continue;
+              }
+            }
+          });
+      },
+        created: function() {
+          this.getCats();
+          this.getToken();
+        }
   }
+}
 </script>
 
 <style scoped>
@@ -68,5 +101,9 @@
       margin:0;
       text-align:center;
       background: linear-gradient(-90deg, #febc55, #ff6969, #9500ff);
-  }
+    }
+    #pfData {
+      font: bold;
+
+    }
 </style>
